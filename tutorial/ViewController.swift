640477308c9611a95df593e7dcfd8ee38f2dc87b
Damien <chickenshalom@gmail.com>
2023-11-23 14:08:56 -0500

    rendering to a view

--- tutorial/ViewController.swift
+++ tutorial/ViewController.swift
@@ -1,19 +1,30 @@
-//
-//  ViewController.swift
-//  tutorial
-//
-//  Created by Damien Afienko on 11/22/23.
-//
-
 import UIKit
+import MetalKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var metalView: MTKView!
 
+    var commandQueue: MTLCommandQueue!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        metalView.device = MTLCreateSystemDefaultDevice()
+        metalView.clearColor = MTLClearColor(red: 0.0, green: 0.0, blue: 1.0, alpha: 1.0)
+        metalView.delegate = self
+        
+        commandQueue = metalView.device?.makeCommandQueue();
     }
-
-
 }
 
+extension ViewController: MTKViewDelegate {
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) { }
+    
+    func draw(in view: MTKView) {
+        let buffer = commandQueue.makeCommandBuffer()!
+        let encoder = buffer.makeRenderCommandEncoder(descriptor: metalView.currentRenderPassDescriptor!)!
+        encoder.endEncoding()
+        buffer.present(view.currentDrawable!)
+        buffer.commit()
+    }
+}
